1ba1ec516efaeb1e7daf45b78c61c8d216330338
Billy Potts <billpottshk@gmail.com>
2014-05-15 20:25:03 +0800

    Fix the paper rock scissors

--- lesson8/labs/rock_paper_scissors/js/main.js
+++ lesson8/labs/rock_paper_scissors/js/main.js
@@ -1,64 +1,63 @@
 var userRock=document.getElementById("rock");
 var userPaper=document.getElementById("paper");
 var userScissors=document.getElementById("scissors");
-var botChoice=Math.random();
+
 //var humanScore=document.getElementById("humanScore")
 //var computerScore=document.getElementById("computerScore")
-var humanScore;
-var computerScore;
+var humanScore = 0;
+var computerScore = 0;
 
-if (botChoice<0.34) {
-  botChoice="rock";  
-} else if (botChoice<= 0.67){
-  botChoice="paper";
-} else {
-  botChoice="scissors";
+
+
+function generateBotChoice() {
+  var botChoice=Math.random();
+  console.log(botChoice);
+  if (botChoice<0.34) {
+    botChoice="rock";  
+  } else if (botChoice<= 0.67){
+    botChoice="paper";
+  } else {
+    botChoice="scissors";
+  }
+  return botChoice;
 }
 
 document.getElementById("rock").onclick=compareRock;
 
 function compareRock() {
 
-// Kit: Try to use console.log() to view the value of a variable on a particular moment
-console.log(botChoice);
+  var botChoice = generateBotChoice();fir
 
-// Kit: Are u doing a comparison here or assignment below?
-if (botChoice=scissors) { 
-  // get the human score
-  humanScore = parseInt(document.getElementById('humanScore').innerHTML);
-  document.getElementById('humanScore').innerHTML = humanScore + 1;
-} else if (botChoice=paper){ // Kit: Are u doing a comparison here or assignment?
-  // Try to apply the same logic as the "botChoice=scissors" case
-  document.getElementById("computerScore").innerHTML=computerScore + 1;
-} else if (botChoice == rock){
-  // Try to apply the same logic as the "botChoice=scissors" case
-  document.getElementById("humanScore").innerHTML = "computerScore" + 0;
-  document.getElementById("computerScore").innerHTML = "computerScore" + 0;
-}
+  // Kit: Try to use console.log() to view the value of a variable on a particular moment
+  console.log(botChoice);
+
+  if (botChoice == 'scissors') {
+    humanScore = humanScore + 1;
+    document.getElementById('humanScore').innerHTML = humanScore;
+  } else if (botChoice == 'paper'){
+    computerScore = computerScore + 1;
+    document.getElementById("computerScore").innerHTML= computerScore;
+  } 
 }
 
 document.getElementById("paper").onclick=comparePaper;
 
 function comparePaper() {
-if (botChoice == scissors) {
- document.getElementById("humanScore").innerHTML = 1;
-} else if (botChoice == rock){
-  document.getElementById('computerScore').innerHTML = "computerScore" + 0;
-} else if (botChoice == paper){
-  document.getElementById('humanScore').innerHTML = "computerScore" + 0;
-  document.getElementById('computerScore').innerHTML = "computerScore" + 0;
-}
+  if (botChoice == scissors) {
+    document.getElementById("humanScore").innerHTML = 1;
+  } else if (botChoice == rock){
+    document.getElementById('computerScore').innerHTML = "computerScore" + 0;
+  } else if (botChoice == paper){
+
+  }
 }
 
 document.getElementById("scissors").onclick=compareScissors;
 
 function compareScissors() {
-if (botChoice == paper) {
- document.getElementById("humanScore").innerHTML = "humanScore" + 1;
-} else if (botChoice == rock){
-  document.getElementById('computerScore').innerHTML = "computerScore" + 1;
-} else if (botChoice == scissors){
-  document.getElementById('humanScore').innerHTML = "computerScore" + 0;
-  document.getElementById('computerScore').innerHTML = "computerScore" + 0;
-}
+  if (botChoice == paper) {
+    document.getElementById("humanScore").innerHTML = "humanScore" + 1;
+  } else if (botChoice == rock){
+    document.getElementById('computerScore').innerHTML = "computerScore" + 1;
+  } 
 }
